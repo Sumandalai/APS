9f696a2966f923902430d2973a250b26a30adba5
Sumandalai <sumandalai2003@gmail.com>
2024-11-06 18:17:07 +0530

    uppercase error

--- app/(auth)/register/page.jsx
+++ app/(auth)/register/page.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { RegisterForm } from '@/components/auth/register-form'
-import { Instructions } from '@/components/auth/instructions'
+import { Instructions } from '@/components/auth/Instructions'
 const RegisterPage = () => {
   return ( 
       <div className="flex justify-center w-[90%]  rounded-lg space-x-8 bg-opacity-95 ">
